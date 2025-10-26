f8c9d99950b6a2d28a8fd4b5191a7c5c9290aa79
kleberAnderson <a.velasco.kleber@gmail.com>
2025-10-26 12:57:04 -0300

    Updating files

--- exercicio-local/SelectsSimples/tarefaSelectsSimples/SQLLocadoraFilme.sql
+++ exercicio-local/SelectsSimples/tarefaSelectsSimples/SQLLocadoraFilme.sql
@@ -1,33 +1,43 @@
+--DELETE DATABASE
+USE master
+GO
+DROP DATABASE LocadoraFilme
+
+--START THE DATABASE
 CREATE DATABASE LocadoraFilme
 GO
 USE LocadoraFilme
+
+--DELETE ENTITY
+--DROP TABLE
+
 GO
 CREATE TABLE Filme(
-	id		INT			NOT NULL,
+	id		INT		IDENTITY(1001, 1),
 	titulo	VARCHAR(40)	NOT NULL,
-	ano		INT			NOT NULL,
+	ano		INT			NOT NULL
 
 	PRIMARY KEY(id)
 )
 GO
 ALTER TABLE Filme
-ALTER COLUMN titulo VARCHAR(80), NOT NULL
+ALTER COLUMN titulo VARCHAR(80) NOT NULL
 
 GO
 CREATE TABLE Estrela(
-	id			INT			NOT NULL,
+	id			INT			NOT NULL	IDENTITY(9901, 1),
 	nome		VARCHAR(40)	NOT NULL
 	PRIMARY KEY(id)
 )
 GO
-ALTER TABLE Estrela ADD nomeReal	VARCHAR(50)
+ALTER TABLE Estrela ADD nomeReal VARCHAR(50)
 
 GO
 CREATE TABLE Filme_Estrela(
-	filmeId		INT		NOT NULL,
+	filmeId		INT,
 	estrelaId	INT		NOT NULL,
 
-	PRIMARY KEY(filmeId, estrelaId)
+	PRIMARY KEY(filmeId, estrelaId),
 
 	FOREIGN KEY(filmeId)
 	REFERENCES Filme(id),
@@ -37,7 +47,7 @@ CREATE TABLE Filme_Estrela(
 )
 GO
 CREATE TABLE Dvd(
-	num				INT,
+	num				INT	IDENTITY(10001, 1),
 	data_Fabricacao	DATE	NOT NULL,
 	filmeId			INT
 	
@@ -48,29 +58,29 @@ CREATE TABLE Dvd(
 )
 GO
 CREATE TABLE Cliente(
-	num_Cadastro	INT,
+	num_Cadastro	INT	IDENTITY(5501, 1),
 	nome			VARCHAR(70)		NOT NULL,
 	logradouro		VARCHAR(150)	NOT NULL,
 	num				INT				NOT NULL	CHECK(num >= 0),
-	cep				CHAR(8)			NOT NULL	CHECK(LEN(cep) = 8)	
+	cep				CHAR(8)			NULL	CHECK(LEN(cep) = 8)	
 
 	PRIMARY KEY(num_Cadastro),
 )
 GO
-CREATLE TABLE Locacao(
+CREATE TABLE Locacao(
 	dvdNum				INT,
 	clienteNum_Cadastro	INT,
 	data_Locacao		DATE	NOT NULL	DEFAULT(GETDATE()),
 	data_Devolucao		DATE			NOT NULL,
 	valor				DECIMAL(7, 2)	NOT NULL	CHECK(valor >= 0)
 
-	PRIMARY KEY(dvdNum, clienteNum_Cadastro, data_Locacao),
+	PRIMARY KEY(data_Locacao, dvdNum, clienteNum_Cadastro),
 
 	FOREIGN KEY(dvdNum)
 	REFERENCES Dvd(num),
 
-	FOREIGN KEY(clienteNum_Cliente)
-	REFERENCES Cliente(num_Cliente)
+	FOREIGN KEY(clienteNum_Cadastro)
+	REFERENCES Cliente(num_Cadastro)
 )
 GO
 INSERT INTO Filme(titulo, ano) VALUES
@@ -89,25 +99,128 @@ INSERT INTO Estrela (nome, nomeReal) VALUES
 ('Steve Carell', 'Steven John Carell'),
 ('Jennifer Garner', 'Jennifer Anne Garner')
 
-GO
-INSERT INTO Dvd(data_Fabricacao) VALUES
-(CONVERT(DATE, '20201202', 112)),
-(CONVERT(DATE, '20191018', 112)),
-(CONVERT(DATE, '20200403', 112)),
-(CONVERT(DATE, '20201202', 112)),
-(CONVERT(DATE, '20191018', 112)),
-(CONVERT(DATE, '20200403', 112)),
-(CONVERT(DATE, '20201202', 112)),
-(CONVERT(DATE, '20191018', 112)),
-(CONVERT(DATE, '20200403', 112))	
+INSERT INTO Filme_Estrela(filmeId, estrelaId) VALUES
+(1002, 9901),
+(1002, 9902),
+(1001, 9903),
+(1005, 9904),
+(1005, 9905)
 
 GO
-INSERT INTO Locacao	(data_Locacao, data_Devolucao, valor) VALUES
-((CONVERT(DATE, '20210218', 112)), (CONVERT(DATE, '20210221', 112)), CAST('3.50' AS DECIMAL(7, 2))),
-((CONVERT(DATE, '20210218', 112)), (CONVERT(DATE, '20210221', 112)), CAST('3.50' AS DECIMAL(7, 2))),
-((CONVERT(DATE, '20210218', 112)), (CONVERT(DATE, '20210219', 112)), CAST('3.50' AS DECIMAL(7, 2))),
-((CONVERT(DATE, '20210220', 112)), (CONVERT(DATE, '20210223', 112)), CAST('3.00' AS DECIMAL(7, 2))),
-((CONVERT(DATE, '20210220', 112)), (CONVERT(DATE, '20210223', 112)), CAST('3.00' AS DECIMAL(7, 2))),
-((CONVERT(DATE, '20210220', 112)), (CONVERT(DATE, '20210223', 112)), CAST('3.00' AS DECIMAL(7, 2))),
-((CONVERT(DATE, '20210224', 112)), (CONVERT(DATE, '20210226', 112)), CAST('3.50' AS DECIMAL(7, 2))),
-((CONVERT(DATE, '20210224', 112)), (CONVERT(DATE, '20210226', 112)), CAST('3.50' AS DECIMAL(7, 2)))
+INSERT INTO Dvd(data_Fabricacao, filmeId) VALUES
+(CONVERT(DATE, '2020/12/02', 102), 1001),
+(CONVERT(DATE, '2019/10/18', 102), 1002),
+(CONVERT(DATE, '2020/04/03', 102), 1003),
+(CONVERT(DATE, '2020/12/02', 102), 1001),
+(CONVERT(DATE, '2019/10/18', 102), 1004),
+(CONVERT(DATE, '2020/04/03', 102), 1002),
+(CONVERT(DATE, '2020/12/02', 102), 1005),
+(CONVERT(DATE, '2019/10/18', 102), 1002),
+(CONVERT(DATE, '2020/04/03', 102), 1003)	
+
+INSERT INTO Cliente(nome, logradouro, num, cep) VALUES
+('Matilde Luz', 'Rua Síria', 150, '03086040'),
+('Carlos Carreiro', 'Rua Bartollomeu', 1250, '04419110'),
+('Matilde Luz', 'Rua Itajutiba', 169, NULL),
+('Matilde Luz', 'Rua Jayme Von Rosenburg', 36, NULL),
+('Matilde Luz', 'Rua Arnaldo Simoes Pinto', 235, '02917110')
+
+GO
+INSERT INTO Locacao	(dvdNum, clienteNum_Cadastro, data_Locacao, data_Devolucao, valor) VALUES
+(10001, 5502,(CONVERT(DATE, '20210218', 102)), (CONVERT(DATE, '20210221', 102)), CAST('3.50' AS DECIMAL(7, 2))),
+(10009, 5502, (CONVERT(DATE, '20210218', 102)), (CONVERT(DATE, '20210221', 102)), CAST('3.50' AS DECIMAL(7, 2))),
+(10002, 5503,(CONVERT(DATE, '20210218', 102)), (CONVERT(DATE, '20210219', 102)), CAST('3.50' AS DECIMAL(7, 2))),
+(10002, 5505,(CONVERT(DATE, '20210220', 102)), (CONVERT(DATE, '20210223', 102)), CAST('3.00' AS DECIMAL(7, 2))),
+(10004, 5505,(CONVERT(DATE, '20210220', 102)), (CONVERT(DATE, '20210223', 102)), CAST('3.00' AS DECIMAL(7, 2))),
+(10005, 5505,(CONVERT(DATE, '20210220', 102)), (CONVERT(DATE, '20210223', 102)), CAST('3.00' AS DECIMAL(7, 2))),
+(10001, 5501,(CONVERT(DATE, '20210224', 102)), (CONVERT(DATE, '20210226', 102)), CAST('3.50' AS DECIMAL(7, 2))),
+(10008, 5501,(CONVERT(DATE, '20210224', 102)), (CONVERT(DATE, '20210226', 102)), CAST('3.50' AS DECIMAL(7, 2)))
+
+--OPERATION
+GO
+UPDATE Cliente
+SET cep = '08411150'
+WHERE num_Cadastro = 5503
+
+GO
+UPDATE Cliente
+SET cep = '02918190'
+WHERE num_Cadastro = 5504
+
+GO
+UPDATE Locacao
+SET valor = 3.25
+WHERE data_Locacao = '2021-02-18' AND clienteNum_Cadastro = 5502
+
+GO
+UPDATE Locacao
+SET valor = 3.10
+WHERE data_Locacao = '2021-02-24' AND clienteNum_Cadastro = 5501
+
+GO
+UPDATE Dvd
+SET data_Fabricacao = '2019-07-14'
+WHERE num = 10005
+
+GO
+UPDATE Estrela
+SET nomeReal = 'Miles Alexander Teller'
+WHERE nome LIKE 'Miles Teller'
+
+--CONSULTATION
+GO
+SELECT titulo AS titulo_Filme
+FROM Filme
+WHERE ano = 2014
+
+GO
+SELECT id, ano
+FROM Filme
+WHERE titulo = 'Birdman'
+
+GO
+SELECT id, ano
+FROM Filme
+WHERE titulo LIKE '%plash'
+
+GO
+SELECT id, nome, nomeReal
+FROM Estrela
+WHERE nome LIKE 'Steve%'
+
+GO
+SELECT filmeId, CONVERT(VARCHAR(10), data_Fabricacao, 103) AS fab
+FROM Dvd
+WHERE data_Fabricacao >= '2020/01/01'
+ORDER BY data_Fabricacao ASC
+
+GO
+SELECT dvdNum, data_Locacao, data_Devolucao, valor, (valor + 2.00) AS 'multa'
+FROM Locacao
+WHERE clienteNum_Cadastro = 5505
+
+GO
+SELECT logradouro, num, cep
+FROM Cliente
+WHERE nome = 'Matilde Luz'
+
+GO
+SELECT nomeReal
+FROM Estrela
+WHERE nome = 'Michael Keaton'
+
+GO
+SELECT num_Cadastro, nome, logradouro + ' ' + CAST(num AS VARCHAR(10)) + ' ' + cep AS 'end_comp'
+FROM Cliente
+WHERE num_Cadastro >= 5503 
+
+EXEC sp_help Users
+EXEC sp_help Projects
+EXEC sp_help Users_has_projects
+
+SELECT * FROM Filme
+SELECT * FROM Estrela
+SELECT * FROM Filme_Estrela
+SELECT * FROM Dvd
+SELECT * FROM Cliente
+SELECT * FROM Locacao
--- exercicio-local/SelectsSimples/tarefaSelectsSimples/SQLProjectsUsers.sql
+++ exercicio-local/SelectsSimples/tarefaSelectsSimples/SQLProjectsUsers.sql
@@ -1,7 +1,37 @@
+--DELETE DATABASE
+USE master
+GO
+DROP DATABASE ProjectsUsers
+
+--START THE DATABASE
 CREATE DATABASE ProjectsUsers
 GO
 USE ProjectsUsers
 
+--DELETE ENTITY
+--DROP TABLE
+
+CREATE TABLE Users (
+	id			INT			NOT NULL	IDENTITY(1, 1),
+	name		VARCHAR(45)	NOT NULL,
+	userName	VARCHAR(45),
+	password	VARCHAR(45)	NULL	DEFAULT ('123mudar'),
+	email		VARCHAR(45)	NOT NULL
+
+	PRIMARY KEY(id)
+)
+
+GO
+CREATE TABLE Projects (
+	id			INT			NOT NULL	IDENTITY(10001, 1),
+	name		VARCHAR(45)	NOT NULL,
+	description	VARCHAR(45),
+	date		DATE		NOT NULL	CHECK(date > '20140901')
+
+	PRIMARY KEY(id)
+)
+
+GO
 CREATE TABLE Users_has_projects (
 	users_Id	INT		NOT NULL,
 	projects_Id	INT		NOT NULL,
@@ -16,44 +46,57 @@ CREATE TABLE Users_has_projects (
 )
 
 GO
+ALTER TABLE Users
+ALTER COLUMN userName VARCHAR(10) NOT NULL
 
-CREATE TABLE Projects (
-	id			INT			NOT NULL	IDENTITY(10001, 1),
-	name		VARCHAR(45)	NOT NULL,
-	description	VARCHAR(45),
-	date		DATE		NOT NULL	CHECK(CONVERT(CHAR(10), (DATE), 103) > 01/09/2014)
-
-	PRIMARY KEY(id)
-)
-
-GO
-
-CREATE TABLE Users (
-	id			INT			NOT NULL	IDENTITY(1, 1),
-	name		VARCHAR(45)	NOT NULL,
-	userName	VARCHAR(45)	NOT NULL	UNIQUE,
-	password	VARCHAR(45)	NOT NULL DEFAULT('123mudar'),
-	email		VARCHAR(45)	NOT NULL
-
-	PRIMARY KEY(id)
-)
 GO
 ALTER TABLE Users
-ALTER COLUMN userName VARCHAR(10)
-GO
-ALTER TABLE Users
-ALTER COLUMN password VARCHAR(8) 
+ALTER COLUMN password VARCHAR(8) NULL	
 
 GO
 INSERT INTO Users (name, userName, password, email) VALUES
-('Maria', 'Rh_maria', NULL, 'maria@empresa.com'),
+('Maria', 'Rh_maria', DEFAULT, 'maria@empresa.com'),
 ('Paulo', 'Ti_paulo', '123@456', 'paulo@empresa.com'),
-('Ana', 'Rh_ana', NULL, 'ana@empresa.com'),
-('Clara', 'Ti_clara', NULL, 'clara@empresa.com'),
+('Ana', 'Rh_ana', DEFAULT, 'ana@empresa.com'),
+('Clara', 'Ti_clara', DEFAULT, 'clara@empresa.com'),
 ('Aparecido', 'Rh_apareci', '55@!cido', 'aparecido@empresa.com')
 
 GO
 INSERT INTO Projects(name, description, date) VALUES
-('Re-folha', 'Refatoração das Folhas', 05/09/2014),
-('Manutenção PCs', 'Manutenção PCs', 06/09/2014),
-('Auditoria', NULL, 07/09/2014)
+('Re-folha', 'Refatoração das Folhas', CONVERT(CHAR(10), '05/09/2014', 103)),
+('Manutencao PCs', 'Manutencao PCs', CONVERT(CHAR(10), '06/09/2014', 103)),
+('Auditoria', NULL, CONVERT(CHAR(10), '07/09/2014', 103))
+
+GO
+INSERT INTO Users_has_projects(users_Id, projects_Id) VALUES
+(1, 10001),
+(5, 10001),
+(3, 10003),
+(4, 10002),
+(2, 10002)
+
+UPDATE Projects
+SET date = CONVERT(CHAR(10), '12/09/2014', 103)
+WHERE name LIKE 'Manutencao%'
+
+GO
+UPDATE Users
+SET userName = 'Rh_cido'
+WHERE name LIKE 'Aparecido'
+
+GO
+UPDATE Users
+SET password = '888@*'
+WHERE userName LIKE 'Rh_maria'
+
+GO
+DELETE Users_has_projects
+WHERE users_Id = 2
+
+EXEC sp_help Users
+EXEC sp_help Projects
+EXEC sp_help Users_has_projects
+
+SELECT * FROM Users
+SELECT * FROM Projects
+SELECT * FROM Users_has_projects
